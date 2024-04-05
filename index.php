b1153d54da4c0f2bd702c9cf60428a196e231360
Fabian <133576299+Fabiano1010@users.noreply.github.com>
2024-04-05 13:47:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="chrome">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalendarz Fabian Skrzypczyński</title>
+    <title>Moon Calendar</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
